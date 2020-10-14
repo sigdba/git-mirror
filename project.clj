e4938361781dafb868088a765189e9dd05ed847d
Dan Boitnott <dboitnot@gmail.com>
2020-10-14 15:44:19 -0500

    Fixed issue with jsch startup https://github.com/clj-commons/clj-ssh/issues/27

--- project.clj
+++ project.clj
@@ -8,10 +8,15 @@
                  [com.fzakaria/slf4j-timbre "0.3.19"]       ; SLF4J logging to timbre
                  [expound "0.8.4"]                          ; Improved spec messages
                  [lambdaisland/uri "1.4.54"]                ; URL parsing
-                 [clj-commons/clj-ssh "0.5.15"]             ; Pure Java SSH client
                  [clj-jgit "1.0.0"]                         ; Pure Java Git client
                  [com.rpl/specter "1.1.3"]                  ; Advanced data manipulation
                  [cheshire "5.10.0"]                        ; JSON parsing
+
+                 ;; Pure Java SSH client
+                 ;; Note: We have to force a later version of jsch because in the Lambda environment we get an error
+                 ;;       at startup because the known_hosts file doesn't exist.
+                 [clj-commons/clj-ssh "0.5.15" :exclusions [com.jcraft/jsch]]
+                 [com.jcraft/jsch "0.1.54"]
 
                  ;; Lambda integration
                  [com.amazonaws/aws-lambda-java-core "1.2.1"]
